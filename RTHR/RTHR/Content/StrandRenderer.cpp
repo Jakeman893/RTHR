3692403cab614a2fe070622f166364530745a085
Jacob Logas <jakeman893@live.com>
2016-02-14 20:57:01 -0500

    Fill functionality for StrandRenderer Class

--- RTHR/RTHR/Content/StrandRenderer.cpp
+++ RTHR/RTHR/Content/StrandRenderer.cpp
@@ -8,6 +8,7 @@ using namespace RTHR;
 using namespace DirectX;
 using namespace Windows::Foundation;
 
+// Loads vertex and pixel shaders from files and instantiates the strand geometry
 StrandRenderer::StrandRenderer(const std::shared_ptr<DX::DeviceResources>& deviceResources) :
 	m_loadingComplete(false),
 	m_indexCount(0),
@@ -18,34 +19,195 @@ StrandRenderer::StrandRenderer(const std::shared_ptr<DX::DeviceResources>& devic
 	CreateWindowSizeDependentResources();
 }
 
+// Initializes view parameters when window size changes
 void StrandRenderer::CreateDeviceDependentResources()
 {
+	//Load shaders asynchronously
+	auto loadVSTask = DX::ReadDataAsync(L"");
+	auto loadPSTask = DX::ReadDataAsync(L"");
+
+	// Once file is loaded, create shader and input layout.
+	auto createVSTask = loadVSTask.then([this](const std::vector<byte>& fileData)
+	{
+		DX::ThrowIfFailed(
+			m_deviceResources->GetD3DDevice()->CreateVertexShader(
+				&fileData[0],
+				fileData.size(),
+				nullptr,
+				&m_vertexShader
+				)
+			);
+
+		static const D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+		{
+			{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+			{"COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA},
+		};
+
+		DX::ThrowIfFailed(
+			m_deviceResources->GetD3DDevice()->CreateInputLayout(
+				vertexDesc,
+				ARRAYSIZE(vertexDesc),
+				&fileData[0],
+				fileData.size(),
+				&m_inputLayout
+				)
+			);
+	});
+
+	//After pixel shader file loaded, create the shader and constant buffer.
+	auto createPSTask = loadPSTask.then([this](const std::vector<byte>& fileData) {
+		DX::ThrowIfFailed(
+			m_deviceResources->GetD3DDevice()->CreatePixelShader(
+				&fileData[0],
+				fileData.size(),
+				nullptr,
+				&m_pixelShader
+				)
+			);
+
+		CD3D11_BUFFER_DESC constantBufferDesc(sizeof(ModelViewProjectionConstantBuffer), D3D11_BIND_CONSTANT_BUFFER);
+		DX::ThrowIfFailed(
+			m_deviceResources->GetD3DDevice()->CreateBuffer(
+				&constantBufferDesc,
+				nullptr,
+				&m_constantBuffer
+				)
+			);
+	});
+
+	// Once both shaders are loaded, create the mesh.
+	auto createMeshTask = (createPSTask && createVSTask).then([this]() {
+
+	});
+
+	// Once mesh is loaded, object is ready to be rendered.
+	createMeshTask.then([this]() {
+		m_loadingComplete = true;
+	});
 }
 
 void StrandRenderer::CreateWindowSizeDependentResources()
 {
+	Size outputSize = m_deviceResources->GetOutputSize();
+	float aspectRatio = outputSize.Width / outputSize.Height;
+	float fovAngleY = 70.0f * XM_PI / 180.0f;
+
+	if (aspectRatio < 1.0f)
+	{
+		fovAngleY *= 2.0f;
+	}
+
+	//This sample makes use of a right-handed coordinate system using row-major matrices
+	XMMATRIX perspectiveMatrix = XMMatrixPerspectiveFovRH(
+		fovAngleY,
+		aspectRatio,
+		0.01f,
+		100.0f
+		);
+
+	XMFLOAT4X4 orientation = m_deviceResources->GetOrientationTransform3D();
+
+	XMMATRIX orientationMatrix = XMLoadFloat4x4(&orientation);
+
+	XMStoreFloat4x4(
+		&m_constantBufferData.projection,
+		XMMatrixTranspose(perspectiveMatrix * orientationMatrix)
+		);
+
+	// Eye at (0,0.7,1.5), looking at point (0,-0.1,0) with the up-vector along the y-axis
+	static const XMVECTORF32 eye = { 0.0f, 0.7f, 1.5f, 0.0f };
+	static const XMVECTORF32 at = { 0.0f, -0.1f, 0.0f, 0.0f };
+	static const XMVECTORF32 up = { 0.0f, 1.0f, 0.0f, 0.0f };
+
+	XMStoreFloat4x4(&m_constantBufferData.view, XMMatrixTranspose(XMMatrixLookAtRH(eye, at, up)));
 }
 
 void StrandRenderer::ReleaseDeviceDependentResources()
 {
+	m_loadingComplete = false;
+	m_vertexShader.Reset();
+	m_inputLayout.Reset();
+	m_pixelShader.Reset();
+	m_constantBuffer.Reset();
+	m_vertexBuffer.Reset();
+	m_indexBuffer.Reset();
 }
 
 void StrandRenderer::Update(DX::StepTimer const & timer)
 {
+
 }
 
 void StrandRenderer::Render()
 {
-}
+	// Loading is asynchronous. Draw geometry after loaded.
+	if (!m_loadingComplete)
+	{
+		return;
+	}
 
-void StrandRenderer::StartTracking()
-{
-}
+	auto context = m_deviceResources->GetD3DDeviceContext();
 
-void StrandRenderer::TrackingUpdate(float positionX)
-{
-}
+	// Prepare the constant buffer to send to the graphics device.
+	context->UpdateSubresource1(
+		m_constantBuffer.Get(),
+		0,
+		NULL,
+		&m_constantBufferData,
+		0,
+		0,
+		0
+		);
 
-void StrandRenderer::StopTracking()
-{
+	//Each vertex is once instance of the VertexPositionColor struct
+	UINT stride = sizeof(VertexPositionColor);
+	UINT offset = 0;
+	context->IASetVertexBuffers(
+		0,
+		1,
+		m_vertexBuffer.GetAddressOf(),
+		&stride,
+		&offset
+		);
+
+	context->IASetIndexBuffer(
+		m_indexBuffer.Get(),
+		DXGI_FORMAT_R16_UINT, // Each index is one 16-bit unsigned integer (short).
+		0
+		);
+
+	context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	context->IASetInputLayout(m_inputLayout.Get());
+
+	// Attach our vertex shader.
+	context->VSSetShader(
+		m_vertexShader.Get(),
+		nullptr,
+		0
+		);
+
+	// Send the constant buffer to graphics device.
+	context->VSSetConstantBuffers1(
+		0,
+		1,
+		m_constantBuffer.GetAddressOf(),
+		nullptr,
+		nullptr
+		);
+
+	//Attach pixel shader.
+	context->PSSetShader(
+		m_pixelShader.Get(),
+		nullptr,
+		0
+		);
+
+	//Draw objects
+	context->DrawIndexed(
+		m_indexCount,
+		0,
+		0
+		);
 }
